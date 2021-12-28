70c6eabaf0122725a2213e1cc1fd089919f83b06
Darío <dariocostapena@gmail.com>
2021-12-28 11:08:03 +0100

    SDL. Drawing a filled triangle

--- src/game.c
+++ src/game.c
@@ -3,42 +3,34 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-const SDL_Color COLOR_TRANSPARENT   = {0};
-const SDL_Color COLOR_BLACK         = {0, 0, 0, 255};
-const SDL_Color COLOR_WHITE         = {255, 255, 255, 255};
-const SDL_Color COLOR_GRAY          = {100, 100, 100, 255};
-const SDL_Color COLOR_DARK_GRAY     = {30, 30, 30, 255};
-const SDL_Color COLOR_LIGHT_GRAY    = {200, 200, 200, 255};
-const SDL_Color COLOR_RED           = {255, 0, 0, 255};
-const SDL_Color COLOR_GREEN         = {0, 255, 0, 255};
-const SDL_Color COLOR_BLUE          = {0, 0, 255, 255};
-const SDL_Color COLOR_YELLOW        = {255, 255, 0, 255};
-const SDL_Color COLOR_ORANGE        = {255, 133, 0, 255};
-const SDL_Color COLOR_PINK          = {255, 0, 206, 255};
-const SDL_Color COLOR_VIOLET        = {145, 0, 255, 255};
-
-void Utils_setBackgroundColor(SDL_Renderer *renderer, SDL_Color color)
-{
-    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
-    SDL_RenderClear(renderer);
-}
-
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
-SDL_Renderer *renderer;
+SDL_Window *window = NULL;
+SDL_Surface *screen_surface = NULL;
+SDL_Renderer *renderer = NULL;
+
+const int TARGET_FPS = 60;
+
+typedef struct Color {
+    Uint8 r;
+    Uint8 g;
+    Uint8 b;
+    Uint8 a;
+} Color;
+
+bool main_loop();
+void close();
 
 int main(int arc, char **argv)
 {
-    printf("Waddup\n");
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         printf(SDL_GetError());
     }
     else
     {
-
-        SDL_Window *window = SDL_CreateWindow(
+        window = SDL_CreateWindow(
             "SDL2 Demo",
             SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
             SCREEN_WIDTH, SCREEN_HEIGHT,
@@ -46,26 +38,30 @@ int main(int arc, char **argv)
 
         if (!window)
         {
-            printf(stderr, "Window could not be created!\n"
+            printf("Window could not be created!\n"
                            "SDL_Error: %s\n",
                    SDL_GetError());
         }
         else
         {
-            renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-            if (!renderer)
-            {
-                printf(stderr, "Renderer could not be created!\n"
-                               "SDL_Error: %s\n",
-                       SDL_GetError());
-            }
-            else
-            {
-                main_loop();
-                SDL_DestroyRenderer(renderer);
-            }
+            screen_surface = SDL_GetWindowSurface(window);
+            renderer = SDL_CreateRenderer(window, -1, 0);
 
-            SDL_DestroyWindow(window);
+            bool quit = false;
+            int lastTime = 0;
+
+            while (!quit)
+            {
+                while (lastTime - SDL_GetTicks() < (1000 / TARGET_FPS))
+                {
+                    SDL_Delay(1);
+                }
+
+                quit = main_loop();
+
+                lastTime = SDL_GetTicks();
+            }
+            close();
         }
 
         SDL_Quit();
@@ -74,36 +70,303 @@ int main(int arc, char **argv)
     return 0;
 }
 
-void main_loop()
+void close()
 {
-    bool quit = false;
+    SDL_DestroyWindow(window);
+    window = NULL;
+    screen_surface = NULL;
+}
 
-    while (!quit)
+void set_pixel(int x, int y, Uint8 r, Uint8 g, Uint8 b)
+{
+    SDL_Surface *surface = screen_surface;
+    Uint32 pixel = SDL_MapRGB(surface->format, r, g, b);
+    Uint32 *const target_pixel = (Uint32 *)((Uint8 *)surface->pixels + y * surface->pitch + x * surface->format->BytesPerPixel);
+    *target_pixel = pixel;
+}
+
+bool main_loop()
+{
+    SDL_Event e;
+
+    while (SDL_PollEvent(&e))
     {
-        SDL_Event e;
-
-        while (SDL_PollEvent(&e))
+        if (e.type == SDL_QUIT)
         {
-            if (e.type == SDL_QUIT)
+            return true;
+        }
+        else if (e.type == SDL_KEYDOWN)
+        {
+            switch (e.key.keysym.sym)
             {
-                quit = true;
-                break;
-            }
-            else if (e.type == SDL_KEYDOWN)
-            {
-                switch (e.key.keysym.sym)
-                {
-                case SDLK_ESCAPE:
-                    quit = true;
-                    break;
-                }
+            case SDLK_ESCAPE:
+                return true;
             }
         }
+    }
 
-        // Set background color
-        Utils_setBackgroundColor(renderer, COLOR_DARK_GRAY);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
 
-        // Update screen
-        SDL_RenderPresent(renderer);
+    // set_pixel(10, 10, 255, 255, 255);
+
+    Color color;
+
+    color.r = 50;
+    color.g = 50;
+    color.b = 255;
+
+    draw_filled_triangle(color, 0, 0, 30, 40, 100, 20);
+
+    SDL_RenderPresent(renderer);
+
+    return false;
+}
+
+void swap(int *x, int *y)
+{
+    int t = *x;
+    *x = *y;
+    *y = t;
+}
+
+void draw_hor_line(Color color, int start_x, int end_x, int y)
+{
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+    SDL_RenderDrawLine(renderer, start_x, y, end_x, y);
+}
+
+void draw_filled_triangle(Color color, int x1, int y1, int x2, int y2, int x3, int y3)
+{
+    int t1x, t2x, y, minx, maxx, t1xp, t2xp;
+    bool changed1 = false;
+    bool changed2 = false;
+    int signx1, signx2, dx1, dy1, dx2, dy2;
+    int e1, e2;
+    // Sort vertices
+    if (y1 > y2)
+    {
+        swap(&y1, &y2);
+        swap(&x1, &x2);
+    }
+    if (y1 > y3)
+    {
+        swap(&y1, &y3);
+        swap(&x1, &x3);
+    }
+    if (y2 > y3)
+    {
+        swap(&y2, &y3);
+        swap(&x2, &x3);
+    }
+
+    t1x = t2x = x1;
+    y = y1; // Starting points
+    dx1 = (int)(x2 - x1);
+    if (dx1 < 0)
+    {
+        dx1 = -dx1;
+        signx1 = -1;
+    }
+    else
+        signx1 = 1;
+    dy1 = (int)(y2 - y1);
+
+    dx2 = (int)(x3 - x1);
+    if (dx2 < 0)
+    {
+        dx2 = -dx2;
+        signx2 = -1;
+    }
+    else
+        signx2 = 1;
+    dy2 = (int)(y3 - y1);
+
+    if (dy1 > dx1)
+    {
+        swap(&dx1, &dy1);
+        changed1 = true;
+    }
+    if (dy2 > dx2)
+    {
+        swap(&dy2, &dx2);
+        changed2 = true;
+    }
+
+    e2 = (int)(dx2 >> 1);
+    // Flat top, just process the second half
+    if (y1 == y2)
+        goto next;
+    e1 = (int)(dx1 >> 1);
+
+    for (int i = 0; i < dx1;)
+    {
+        t1xp = 0;
+        t2xp = 0;
+        if (t1x < t2x)
+        {
+            minx = t1x;
+            maxx = t2x;
+        }
+        else
+        {
+            minx = t2x;
+            maxx = t1x;
+        }
+        // process first line until y value is about to change
+        while (i < dx1)
+        {
+            i++;
+            e1 += dy1;
+            while (e1 >= dx1)
+            {
+                e1 -= dx1;
+                if (changed1)
+                    t1xp = signx1; // t1x += signx1;
+                else
+                    goto next1;
+            }
+            if (changed1)
+                break;
+            else
+                t1x += signx1;
+        }
+        // Move line
+    next1:
+        // process second line until y value is about to change
+        while (1)
+        {
+            e2 += dy2;
+            while (e2 >= dx2)
+            {
+                e2 -= dx2;
+                if (changed2)
+                    t2xp = signx2; // t2x += signx2;
+                else
+                    goto next2;
+            }
+            if (changed2)
+                break;
+            else
+                t2x += signx2;
+        }
+    next2:
+        if (minx > t1x)
+            minx = t1x;
+        if (minx > t2x)
+            minx = t2x;
+        if (maxx < t1x)
+            maxx = t1x;
+        if (maxx < t2x)
+            maxx = t2x;
+        draw_hor_line(color, minx, maxx, y); // Draw line from min to max points found on the y
+                                  // Now increase y
+        if (!changed1)
+            t1x += signx1;
+        t1x += t1xp;
+        if (!changed2)
+            t2x += signx2;
+        t2x += t2xp;
+        y += 1;
+        if (y == y2)
+            break;
+    }
+next:
+    // Second half
+    dx1 = (int)(x3 - x2);
+    if (dx1 < 0)
+    {
+        dx1 = -dx1;
+        signx1 = -1;
+    }
+    else
+        signx1 = 1;
+    dy1 = (int)(y3 - y2);
+    t1x = x2;
+
+    if (dy1 > dx1)
+    {
+        swap(&dy1, &dx1);
+        changed1 = true;
+    }
+    else
+        changed1 = false;
+
+    e1 = (int)(dx1 >> 1);
+
+    for (int i = 0; i <= dx1; i++)
+    {
+        t1xp = 0;
+        t2xp = 0;
+        if (t1x < t2x)
+        {
+            minx = t1x;
+            maxx = t2x;
+        }
+        else
+        {
+            minx = t2x;
+            maxx = t1x;
+        }
+        // process first line until y value is about to change
+        while (i < dx1)
+        {
+            e1 += dy1;
+            while (e1 >= dx1)
+            {
+                e1 -= dx1;
+                if (changed1)
+                {
+                    t1xp = signx1;
+                    break;
+                } // t1x += signx1;
+                else
+                    goto next3;
+            }
+            if (changed1)
+                break;
+            else
+                t1x += signx1;
+            if (i < dx1)
+                i++;
+        }
+    next3:
+        // process second line until y value is about to change
+        while (t2x != x3)
+        {
+            e2 += dy2;
+            while (e2 >= dx2)
+            {
+                e2 -= dx2;
+                if (changed2)
+                    t2xp = signx2;
+                else
+                    goto next4;
+            }
+            if (changed2)
+                break;
+            else
+                t2x += signx2;
+        }
+    next4:
+
+        if (minx > t1x)
+            minx = t1x;
+        if (minx > t2x)
+            minx = t2x;
+        if (maxx < t1x)
+            maxx = t1x;
+        if (maxx < t2x)
+            maxx = t2x;
+        draw_hor_line(color, minx, maxx, y);
+        if (!changed1)
+            t1x += signx1;
+        t1x += t1xp;
+        if (!changed2)
+            t2x += signx2;
+        t2x += t2xp;
+        y += 1;
+        if (y > y3)
+            return;
     }
 }
